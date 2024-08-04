11d98804026e17bcc388179f89791f664eacc5bb
SUMALESH K A <167864084+SUMALESH-KA@users.noreply.github.com>
2024-08-04 22:00:00 +0530

    Create 326.Power-of-three.c

---
+++ 326.Power-of-three.c
@@ -0,0 +1,15 @@
+bool isPowerOfThree(int n) {
+    
+    while(n > 0){
+        if(n==1){
+            return true;
+        }
+
+        if(n%3!=0){
+            return false;
+        }
+
+        n=n/3;
+    }
+    return false;
+}
